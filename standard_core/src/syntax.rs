37ef3577eef47336d3aaf2b4e7131f0a9f4b6f96
PhillY <philly706869@gmail.com>
2025-09-19 20:24:59 +0000

    2025-09-20: UAX #31

--- standard_core/src/syntax.rs
+++ standard_core/src/syntax.rs
@@ -11,7 +11,22 @@ pub trait Syntax<'i> {
     fn parse(&self, input: &'i str) -> Result<Self::Output, Self::Error>;
 }
 
-/// Identifier syntax based on [Unicode Standard Annex #31](https://www.unicode.org/reports/tr31/tr31-43.html)
+/// Identifier syntax conforms to [UAX #31](https://www.unicode.org/reports/tr31/tr31-43.html),
+/// using [R1-2](https://www.unicode.org/reports/tr31/tr31-43.html#R1-2) with the following profile:
+///
+/// **Base Definition**
+/// - Start characters = `ID_Start`
+/// - Continue characters = `ID_Continue`
+/// - Normalization form = NFC
+///
+/// **Adopted Standard Profiles**
+/// - [Mathematical Compatibility Notation Profile](https://www.unicode.org/reports/tr31/tr31-43.html#Mathematical_Compatibility_Notation_Profile)
+/// - [Emoji Profile](https://www.unicode.org/reports/tr31/tr31-43.html#Emoji_Profile)
+/// - [Default-Ignorable Exclusion Profile](https://www.unicode.org/reports/tr31/tr31-43.html#Default_Ignorable_Exclusion_Profile)
+///
+/// **Language-specific Extensions**
+/// - Add U+0024 DOLLAR SIGN ($) to Start and Continue
+/// - Add U+005F LOW LINE (_) to Start and Continue
 pub struct IdentifierSyntax;
 
 impl IdentifierSyntax {
@@ -19,43 +34,63 @@ impl IdentifierSyntax {
         let category = get_general_category(chr);
         matches!(
             category,
-            UC::UppercaseLetter
-                | UC::LowercaseLetter
-                | UC::TitlecaseLetter
-                | UC::ModifierLetter
-                | UC::OtherLetter
-                | UC::LetterNumber
-        )
+            UC::UppercaseLetter | // Lu
+            UC::LowercaseLetter | // Ll
+            UC::TitlecaseLetter | // Lt
+            UC::ModifierLetter | // Lm
+            UC::OtherLetter | // Lo
+            UC::LetterNumber // Nl
+        ) || Self::match_other_id_start(chr)
+            || Self::match_id_compat_math_start(chr)
     }
 
     fn match_id_continue(chr: char) -> bool {
         let category = get_general_category(chr);
-        Self::match_id_start(chr) || matches!(category, UC::UppercaseLetter)
+        Self::match_id_start(chr)
+            || matches!(
+                category,
+                UC::NonspacingMark | // Mn
+                UC::SpacingMark | // Mc
+                UC::DecimalNumber | // Nd 
+                UC::ConnectorPunctuation // Pc
+            )
+            || Self::match_other_id_continue(chr)
+            || Self::match_id_compat_math_continue(chr)
     }
 
-    fn match_xid_start(chr: char) -> bool {
-        let category = get_general_category(chr);
-        todo!()
-    }
+    // fn match_xid_start(chr: char) -> bool {
+    //     let category = get_general_category(chr);
+    //     todo!()
+    // }
 
-    fn match_xid_continue(chr: char) -> bool {
-        let category = get_general_category(chr);
-        todo!()
-    }
+    // fn match_xid_continue(chr: char) -> bool {
+    //     let category = get_general_category(chr);
+    //     todo!()
+    // }
 
     fn match_other_id_start(chr: char) -> bool {
-        todo!()
+        matches!(chr, '\u{2118}' | '\u{212E}' | '\u{309B}' | '\u{309C}')
     }
 
     fn match_other_id_continue(chr: char) -> bool {
+        matches!(chr, '\u{1369}' | '\u{00B7}' | '\u{0387}' | '\u{19DA}')
+    }
+
+    fn match_id_compat_math_start(chr: char) -> bool {
+        todo!()
+    }
+
+    fn match_id_compat_math_continue(chr: char) -> bool {
         todo!()
     }
 
     fn match_pattern_syntax(chr: char) -> bool {
+        let category = get_general_category(chr);
         todo!()
     }
 
     fn match_pattern_white_space(chr: char) -> bool {
+        let category = get_general_category(chr);
         todo!()
     }
 }
