c60610a2b58a85ddb57ae15aaddf6a49b8cfcbe2
PhillY <philly706869@gmail.com>
2025-10-03 18:56:19 +0000

    2025-10-04

--- core/src/acm.rs
+++ core/src/acm.rs
@@ -3,12 +3,14 @@
 mod block;
 mod function;
 mod global;
+mod instruction;
 mod module;
 mod uid;
 
 pub use block::Block;
 pub use function::Function;
 pub use global::Global;
+pub use instruction::Instruction;
 pub use module::Module;
 
 use std::collections::HashSet;
---
+++ core/src/acm/instruction.rs
@@ -0,0 +1,5 @@
+pub enum Instruction {
+    Return,
+}
+
+impl Instruction {}
