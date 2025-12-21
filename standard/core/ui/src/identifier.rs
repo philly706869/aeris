6da6f07cc39ea7a40e9883fe2ed91f188bf0d13c
PhillY <philly706869@gmail.com>
2025-12-21 11:51:37 +0000

    2025-12-21 2

--- standard/core/ui/src/identifier.rs
+++ standard/core/ui/src/identifier.rs
@@ -1,39 +1,39 @@
-use icu_properties::{
-    CodePointSetData,
-    props::{IdContinue, IdStart},
-};
+// use icu_properties::{
+//     CodePointSetData,
+//     props::{IdContinue, IdStart},
+// };
 
 // use crate::ui::{UIParseContext, UIParseErr, UIParser, UISyntax};
 
-/// Identifier syntax conforms to [UAX #31](https://www.unicode.org/reports/tr31/tr31-43.html),
-/// using [R1-2](https://www.unicode.org/reports/tr31/tr31-43.html#R1-2) with the following profile:
-///
-/// **Base Definition**
-/// - Start characters = `ID_Start`
-/// - Continue characters = `ID_Continue`
-/// - Normalization form = NFC
-///
-/// **Adopted Standard Profiles**
-// /// - [Mathematical Compatibility Notation Profile](https://www.unicode.org/reports/tr31/tr31-43.html#Mathematical_Compatibility_Notation_Profile)
-// /// - [Emoji Profile](https://www.unicode.org/reports/tr31/tr31-43.html#Emoji_Profile)
-// /// - [Default-Ignorable Exclusion Profile](https://www.unicode.org/reports/tr31/tr31-43.html#Default_Ignorable_Exclusion_Profile)
-///
-/// **Language-specific Extensions**
-/// - Add U+0024 DOLLAR SIGN ($) to Start and Continue
-/// - Add U+005F LOW LINE (_) to Start and Continue
-pub struct Identifier;
+// /// Identifier syntax conforms to [UAX #31](https://www.unicode.org/reports/tr31/tr31-43.html),
+// /// using [R1-2](https://www.unicode.org/reports/tr31/tr31-43.html#R1-2) with the following profile:
+// ///
+// /// **Base Definition**
+// /// - Start characters = `ID_Start`
+// /// - Continue characters = `ID_Continue`
+// /// - Normalization form = NFC
+// ///
+// /// **Adopted Standard Profiles**
+// // /// - [Mathematical Compatibility Notation Profile](https://www.unicode.org/reports/tr31/tr31-43.html#Mathematical_Compatibility_Notation_Profile)
+// // /// - [Emoji Profile](https://www.unicode.org/reports/tr31/tr31-43.html#Emoji_Profile)
+// // /// - [Default-Ignorable Exclusion Profile](https://www.unicode.org/reports/tr31/tr31-43.html#Default_Ignorable_Exclusion_Profile)
+// ///
+// /// **Language-specific Extensions**
+// /// - Add U+0024 DOLLAR SIGN ($) to Start and Continue
+// /// - Add U+005F LOW LINE (_) to Start and Continue
+// pub struct Identifier;
 
-impl Identifier {
-    fn match_start(ch: char) -> bool {
-        let id_start_set = CodePointSetData::new::<IdStart>();
-        id_start_set.contains(ch) || matches!(ch, '$' | '_')
-    }
+// impl Identifier {
+//     fn match_start(ch: char) -> bool {
+//         let id_start_set = CodePointSetData::new::<IdStart>();
+//         id_start_set.contains(ch) || matches!(ch, '$' | '_')
+//     }
 
-    fn match_continue(ch: char) -> bool {
-        let id_continue_set = CodePointSetData::new::<IdContinue>();
-        id_continue_set.contains(ch)
-    }
-}
+//     fn match_continue(ch: char) -> bool {
+//         let id_continue_set = CodePointSetData::new::<IdContinue>();
+//         id_continue_set.contains(ch)
+//     }
+// }
 
 // impl UISyntax for Identifier {
 //     type Parser = Self;
