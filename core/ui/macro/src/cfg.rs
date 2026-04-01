85de15ad3a49bd96839fcfbb6602873246601a5c
PhillY <philly706869@gmail.com>
2026-04-01 09:34:59 +0000

    2026-04-01

---
+++ core/ui/macro/src/cfg.rs
@@ -0,0 +1,5 @@
+use proc_macro2::TokenStream;
+
+pub fn cfg(token_stream: TokenStream) -> TokenStream {
+    todo!()
+}
--- core/ui/macro/src/lib.rs
+++ core/ui/macro/src/lib.rs
@@ -1,1 +1,8 @@
+mod cfg;
 
+use proc_macro::TokenStream;
+
+#[proc_macro]
+pub fn cfg(token_stream: TokenStream) -> TokenStream {
+    cfg::cfg(token_stream.into()).into()
+}
