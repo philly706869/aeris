39ff3600b33d39650fa975233aae3eafc11e1ac8
PhillY <philly706869@gmail.com>
2026-04-13 11:48:09 +0000

    2026-04-13 6

--- core/ui/macro/src/lib.rs
+++ core/ui/macro/src/lib.rs
@@ -1,14 +1,38 @@
 use proc_macro::TokenStream;
 
 #[proc_macro]
-pub fn token(stream: TokenStream) -> TokenStream {
-    lib::token(stream.into()).into()
+pub fn token(input: TokenStream) -> TokenStream {
+    match lib::token(input.into()) {
+        Ok(expanded) => expanded.into(),
+        Err(err) => err.to_compile_error().into(),
+    }
 }
 
 mod lib {
     use proc_macro2::TokenStream;
+    use quote::quote;
+    use syn::{
+        Ident,
+        parse::{Parse, ParseStream},
+        parse2,
+    };
 
-    pub fn token(stream: TokenStream) -> TokenStream {
-        todo!()
+    struct Data {
+        name: Ident,
+    }
+
+    impl Parse for Data {
+        fn parse(input: ParseStream) -> syn::Result<Self> {
+            let name: Ident = input.parse()?;
+            Ok(Self { name })
+        }
+    }
+
+    pub fn token(input: TokenStream) -> syn::Result<TokenStream> {
+        let data: Data = parse2(input)?;
+
+        let expanded = quote! {};
+
+        Ok(expanded)
     }
 }
