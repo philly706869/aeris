7b034ab30817d0125ec9b2ec5d8905de1da7b074
PhillY <philly706869@gmail.com>
2026-04-13 09:27:01 +0000

    2026-04-13 5 LLVM 22 update

--- core/ui/macro/src/lib.rs
+++ core/ui/macro/src/lib.rs
@@ -1,1 +1,14 @@
+use proc_macro::TokenStream;
 
+#[proc_macro]
+pub fn token(stream: TokenStream) -> TokenStream {
+    lib::token(stream.into()).into()
+}
+
+mod lib {
+    use proc_macro2::TokenStream;
+
+    pub fn token(stream: TokenStream) -> TokenStream {
+        todo!()
+    }
+}
