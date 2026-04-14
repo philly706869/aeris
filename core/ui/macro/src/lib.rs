2991708e683cfd84944a1f429efffcffaccd776f
PhillY <philly706869@gmail.com>
2026-04-14 09:08:48 +0000

    2026-04-14 2

--- core/ui/macro/src/lib.rs
+++ core/ui/macro/src/lib.rs
@@ -1,38 +1,25 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{
+    Ident,
+    parse::{Parse, ParseStream},
+    parse_macro_input,
+};
 
-#[proc_macro]
-pub fn token(input: TokenStream) -> TokenStream {
-    match lib::token(input.into()) {
-        Ok(expanded) => expanded.into(),
-        Err(err) => err.to_compile_error().into(),
+struct TokenData {
+    name: Ident,
+}
+
+impl Parse for TokenData {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let name: Ident = input.parse()?;
+        Ok(Self { name })
     }
 }
 
-mod lib {
-    use proc_macro2::TokenStream;
-    use quote::quote;
-    use syn::{
-        Ident,
-        parse::{Parse, ParseStream},
-        parse2,
-    };
+#[proc_macro]
+pub fn token(input: TokenStream) -> TokenStream {
+    let data = parse_macro_input!(input as TokenData);
 
-    struct Data {
-        name: Ident,
-    }
-
-    impl Parse for Data {
-        fn parse(input: ParseStream) -> syn::Result<Self> {
-            let name: Ident = input.parse()?;
-            Ok(Self { name })
-        }
-    }
-
-    pub fn token(input: TokenStream) -> syn::Result<TokenStream> {
-        let data: Data = parse2(input)?;
-
-        let expanded = quote! {};
-
-        Ok(expanded)
-    }
+    quote! {}.into()
 }
