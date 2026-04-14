3e0db6c2d68132858368b6fd48f67f002e28f483
PhillY <philly706869@gmail.com>
2026-04-14 13:34:04 +0000

    2026-04-14 4

--- core/ui/src/lib.rs
+++ core/ui/src/lib.rs
@@ -1,8 +1,1 @@
-use pest::Parser;
-use pest_derive::Parser;
 
-#[derive(Parser)]
-#[grammar = "syntax/comment.pest"]
-#[grammar = "syntax/identifier.pest"]
-#[grammar = "syntax/keyword.pest"]
-pub struct AERISParser;
