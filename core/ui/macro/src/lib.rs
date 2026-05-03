e406a1092e7698fdee54b37225c2489dc6f56bce
PhillY <philly706869@gmail.com>
2026-05-03 10:51:53 +0000

    2026-05-03 4

--- core/ui/macro/src/lib.rs
+++ core/ui/macro/src/lib.rs
@@ -5,14 +5,60 @@ use syn::{
     parse_macro_input,
 };
 
+/*
+NCFG
+extern Ident RArrow LitStr
+
+Definition -> Pub | Rule
+Pub -> "pub" Ident
+Rule -> Ident "->" Item
+Item -> Ident | Ident ":" Item | LitStr | "{" "}"
+
+DCFG
+
+*/
+
 struct SyntaxDefinition {}
+
+enum Statement {
+    Definition,
+    PubIdent,
+    RuleArrow,
+    Rule,
+}
 
 impl Parse for SyntaxDefinition {
     fn parse(stream: ParseStream) -> syn::Result<Self> {
+        let mut state = Statement::Definition;
         let mut pubs: Vec<Ident> = Vec::new();
         let mut rules: Vec<Ident> = Vec::new();
 
         loop {
+            match state {
+                Statement::Definition => {
+                    if stream.parse::<Token![pub]>().is_ok() {
+                        state = Statement::PubIdent;
+                    } else if let Ok(ident) = stream.parse::<Ident>() {
+                        state = Statement::RuleArrow;
+                    } else {
+                        break;
+                    }
+                }
+                Statement::PubIdent => {
+                    if let Ok(ident) = stream.parse::<Ident>() {
+                    } else if let Ok(lit_str) = stream.parse::<LitStr>() {
+                    } else {
+                        break;
+                    }
+                }
+                Statement::RuleArrow => {
+                    if stream.parse::<Token![->]>().is_ok() {
+                    } else {
+                        break;
+                    }
+                }
+                Statement::Rule => {}
+            }
             if stream.parse::<Token![pub]>().is_ok() {
                 pubs.push(stream.parse()?);
             } else if let Ok(ident) = stream.parse::<Ident>() {
