531c17c9451cab2af886a220e487cba5f71f9081
PhillY <philly706869@gmail.com>
2026-05-17 16:44:57 +0000

    2026-05-18 1

--- core/ui/macro/src/lib.rs
+++ core/ui/macro/src/lib.rs
@@ -7,15 +7,15 @@ use syn::parse_macro_input;
 
 #[proc_macro]
 pub fn syntax(stream: TokenStream) -> TokenStream {
-    let syntax = parse_macro_input!(stream as ast::Syntax);
-    let mut pubs = Vec::new();
-    let mut rules = Vec::new();
-    for statement in syntax.statements {
-        match statement {
-            ast::Statement::PubDecl(pub_decl) => pubs.push(pub_decl),
-            ast::Statement::RuleDef(rule_def) => rules.push(rule_def),
-        }
-    }
+    // let syntax = parse_macro_input!(stream as ast::Syntax);
+    // let mut pubs = Vec::new();
+    // let mut rules = Vec::new();
+    // for statement in syntax.statements {
+    //     match statement {
+    //         ast::Statement::PubDecl(pub_decl) => pubs.push(pub_decl),
+    //         ast::Statement::RuleDef(rule_def) => rules.push(rule_def),
+    //     }
+    // }
 
     TokenStream::new()
 }
