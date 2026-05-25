4d8e13f4f6c6f8e3f778e12787dd8b9137427fb5
PhillY <philly706869@gmail.com>
2026-05-25 15:57:14 +0000

    2026-05-26 1

--- core/ui/tests/example.rs
+++ core/ui/tests/example.rs
@@ -1,17 +1,19 @@
 mod syntax;
 
-mod prototype {
+mod proto_json {
     use aeris_ui::syntax;
 
     syntax! {
-        {
-            pub NestedA;
-            NestedA -> "foo";
-            NestedA -> NestedB;
-            {
-                pub NestedB;
-                NestedB -> "bar";
-            }
+        struct JSONWhiteSpace -> (" " | "\t" | "\n" | "\r")*;
+    }
+
+    // use self::JSONWhiteSpace as S
+
+    syntax! {
+        pub enum JSONNumber {
+            Int -> int:Digit+;
+            Float -> int:Digit+ p:"." frac:Digit+;
         }
+        Digit -> ['0'..'9'];
     }
 }
