e6fe423aa1e4d28e6e4bfaed1ad80a7d20e29dc2
PhillY <philly706869@gmail.com>
2026-05-26 18:51:49 +0000

    2026-05-27 1

--- core/ui/tests/example.rs
+++ core/ui/tests/example.rs
@@ -1,19 +1,68 @@
 mod syntax;
 
 mod proto_json {
-    use aeris_ui::syntax;
+    mod syntax {
+        use aeris_ui::syntax;
 
-    syntax! {
-        struct JSONWhiteSpace -> (" " | "\t" | "\n" | "\r")*;
-    }
-
-    // use self::JSONWhiteSpace as S
-
-    syntax! {
-        pub enum JSONNumber {
-            Int -> int:Digit+;
-            Float -> int:Digit+ p:"." frac:Digit+;
+        syntax! {
+            pub struct JSON {
+                [ws]:WS
+                value:JSONValue
+                [ws]:WS
+            }
         }
-        Digit -> ['0'..'9'];
+
+        syntax! {
+            pub enum JSONValue {
+                Object ( JSONObject )
+                Array ( JSONArray )
+                String ( JSONString )
+                Number ( JSONNumber )
+                Boolean ( JSONBoolean )
+                Null ( JSONNull )
+            }
+        }
+
+        syntax! {
+            pub struct JSONObject (
+
+            )
+        }
+
+        syntax! {
+            pub struct JSONArray
+        }
+
+        syntax! {
+            pub struct JSONString { qs:'"' content:Content qe:'"' }
+            Content ( ([! '\\' '\u{0000}'..'\u{001F}'] | '\\' Escape)* )
+            Escape ( '"' | '\\' | '/' | 'b' | 'f' | 'n' | 'r' | 't' | ('u' ['0'..'9' 'A'..'F' 'a'..'f']{4}) )
+        }
+
+        syntax! {
+            pub struct JSONNumber {
+                sign:'-'?
+                integer:(Digit | One2Nine Digits)
+                fraction:()
+            }
+            Digits ( Digit+ )
+            Digit ( '0' | One2Nine )
+            One2Nine ( ['1'..'9'] )
+        }
+
+        syntax! {
+            pub enum JSONBoolean {
+                True ( "true" )
+                False ( "false" )
+            }
+        }
+
+        syntax! {
+            pub struct JSONNull ( "null" )
+        }
+
+        syntax! {
+            struct WS ( (" " | "\t" | "\n" | "\r")* )
+        }
     }
 }
