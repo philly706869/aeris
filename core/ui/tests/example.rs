922279a9a1997bd60f281159043b721bb841a1e6
PhillY <philly706869@gmail.com>
2026-06-02 17:44:02 +0000

    2026-06-03 2

--- core/ui/tests/example.rs
+++ core/ui/tests/example.rs
@@ -1,14 +1,15 @@
 mod syntax;
 
+// Ready for implement
 mod proto_json {
     mod syntax {
         use aeris_ui::syntax;
 
         syntax! {
             pub struct JSON {
-                (ws):WS
-                value:JSONValue
-                (ws):WS
+                (ws): WS
+                value: JSONValue
+                (ws): WS
             }
         }
 
@@ -24,30 +25,107 @@ mod proto_json {
         }
 
         syntax! {
-            pub struct JSONObject (
+            pub struct JSONObject {
+                (brace): '{'
+                content: JSONObjectContent
+                (brace): '}'
+            }
 
+            pub enum JSONObjectContent {
+                None ( WS )
+                Some {
+                    first: JSONObjectEntry
+                    rest: JSONObjectRestEntry*
+                }
+            }
+
+            pub struct JSONObjectRestEntry {
+                rest: ','
+                entry: JSONObjectEntry
+            }
+
+            pub struct JSONObjectEntry {
+                (ws): WS
+                name: JSONString
+                (ws): WS
+                colon: ':'
+                (ws): WS
+                value: JSONValue
+                (ws): WS
+            }
+        }
+
+        syntax! {
+            pub struct JSONArray {
+                (bracket): '['
+                content: JSONArrayContent
+                (bracket): ']'
+            }
+
+            pub enum JSONArrayContent {
+                None ( WS )
+                Some {
+                    first: JSONArrayEntry
+                    rest: JSONArrayRestEntry*
+                }
+            }
+
+            pub struct JSONArrayRestEntry {
+                rest: ','
+                entry: JSONArrayEntry
+            }
+
+            pub struct JSONArrayEntry {
+                (ws): WS
+                value: JSONValue
+                (ws): WS
+            }
+        }
+
+        syntax! {
+            pub struct JSONString {
+                (quote): '"'
+                content: Content
+                (quote): '"'
+            }
+            Content (
+                (
+                    | {! '"' '\\' '\u{0000}'..'\u{001F}'}
+                    | '\\' Escape
+                )*
+            )
+            Escape (
+                {'"' '\\' '/' 'b' 'f' 'n' 'r' 't'} | ('u' {'0'..'9' 'A'..'F' 'a'..'f'}[4])
             )
         }
 
         syntax! {
-            pub struct JSONArray
-        }
-
-        syntax! {
-            pub struct JSONString { qs:'"' content:Content qe:'"' }
-            Content ( ([! '\\' '\u{0000}'..'\u{001F}'] | '\\' Escape)* )
-            Escape ( '"' | '\\' | '/' | 'b' | 'f' | 'n' | 'r' | 't' | ('u' ['0'..'9' 'A'..'F' 'a'..'f']{4}) )
-        }
-
-        syntax! {
             pub struct JSONNumber {
-                sign:'-'?
-                integer:(Digit | One2Nine Digits)
-                fraction:()
+                sign: '-'?
+                integer: (Digit | One2Nine Digits)
+                fraction: JSONFraction?
+                exponent: JSONExponent?
             }
+
+            pub struct JSONFraction {
+                point: '.'
+                digits: Digits
+            }
+
+            pub struct JSONExponent {
+                e: {'E' 'e'}
+                sign: JSONSign?
+                digits: Digits
+            }
+
+            pub enum JSONSign {
+                Plus ( '+' )
+                Minus ( '-' )
+            }
+
             Digits ( Digit+ )
             Digit ( '0' | One2Nine )
-            One2Nine ( ['1'..'9'] )
+            One2Nine ( {'1'..'9'} )
         }
 
         syntax! {
@@ -62,7 +140,7 @@ mod proto_json {
         }
 
         syntax! {
-            struct WS ( (" " | "\t" | "\n" | "\r")* )
+            struct WS ( {' ' '\t' '\n' '\r'}* )
         }
     }
 }
