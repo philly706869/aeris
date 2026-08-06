5fa31a4ce60b475b5c10013ebcd4718cbdab378d
PhillY <philly706869@gmail.com>
2026-08-06 16:06:18 +0000

    2026-08-07 1

--- crates/core/ui/proc/src/ast.rs
+++ crates/core/ui/proc/src/ast.rs
@@ -1,82 +1,125 @@
 use syn::{
     Attribute, Ident, LitChar, LitInt, LitStr, Token, Visibility, braced, bracketed, parenthesized,
-    parse::{Parse, ParseStream},
-    token::{Brace, Bracket, Paren},
+    parse::{Parse, ParseStream, discouraged::Speculative},
+    token::Bracket,
 };
 
 #[derive(Debug)]
 pub struct ClusterAST {
     pub shards: Vec<ShardAST>,
-    pub lambdas: Vec<LambdaAST>,
 }
 
 impl Parse for ClusterAST {
     fn parse(input: ParseStream) -> syn::Result<Self> {
         let mut shards = Vec::new();
-        let mut lambdas = Vec::new();
         while !input.is_empty() {
-            if input.peek(Ident) {
-                let name = input.parse()?;
-                let entry = input.parse()?;
-                lambdas.push(LambdaAST { name, entry });
-                continue;
-            }
-
-            let attrs = Attribute::parse_outer(input)?;
-            let vis = input.parse()?;
-            let lookahead1 = input.lookahead1();
-            let is_struct = if lookahead1.peek(Token![struct]) {
-                input.parse::<Token![struct]>()?;
-                true
-            } else if lookahead1.peek(Token![enum]) {
-                input.parse::<Token![enum]>()?;
-                false
-            } else {
-                return Err(lookahead1.error());
-            };
-
-            let name = input.parse()?;
-            input.parse::<Token![trait]>()?;
-            let trait_name = input.parse()?;
-            let shape = if is_struct {
-                ShapeAST::Struct(input.parse()?)
-            } else {
-                let content;
-                braced!(content in input);
-                let mut variants = Vec::new();
-                while !content.is_empty() {
-                    let name = content.parse()?;
-                    let sequence = content.parse()?;
-                    variants.push((name, sequence));
-                }
-                ShapeAST::Enum(variants)
-            };
-
-            shards.push(ShardAST {
-                attrs,
-                vis,
-                name,
-                trait_name,
-                shape,
-            });
+            shards.push(input.parse()?);
         }
-        Ok(Self { shards, lambdas })
+        Ok(Self { shards })
     }
 }
 
 #[derive(Debug)]
-pub struct ShardAST {
+pub enum ShardAST {
+    Struct(StructAST),
+    Enum(EnumAST),
+    Lambda(LambdaAST),
+}
+
+impl Parse for ShardAST {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let fork = input.fork();
+        if let Ok(struct_) = fork.parse::<StructAST>() {
+            input.advance_to(&fork);
+            return Ok(Self::Struct(struct_));
+        }
+        let fork = input.fork();
+        if let Ok(enum_) = fork.parse::<EnumAST>() {
+            input.advance_to(&fork);
+            return Ok(Self::Enum(enum_));
+        }
+        let fork = input.fork();
+        if let Ok(lambda) = fork.parse::<LambdaAST>() {
+            input.advance_to(&fork);
+            return Ok(Self::Lambda(lambda));
+        }
+        Err(input.error("expected struct, enum, or lambda"))
+    }
+}
+
+#[derive(Debug)]
+pub struct StructAST {
     pub attrs: Vec<Attribute>,
     pub vis: Visibility,
     pub name: Ident,
     pub trait_name: Ident,
-    pub shape: ShapeAST,
+    pub sequence: SequenceAST,
+}
+
+impl Parse for StructAST {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let attrs = Attribute::parse_outer(input)?;
+        let vis = input.parse()?;
+        input.parse::<Token![struct]>()?;
+        let name = input.parse()?;
+        input.parse::<Token![trait]>()?;
+        let trait_name = input.parse()?;
+        let sequence = input.parse()?;
+        Ok(Self {
+            attrs,
+            vis,
+            name,
+            trait_name,
+            sequence,
+        })
+    }
 }
 
 #[derive(Debug)]
-pub enum ShapeAST {
-    Struct(SequenceAST),
-    Enum(Vec<(Ident, SequenceAST)>),
+pub struct EnumAST {
+    pub attrs: Vec<Attribute>,
+    pub vis: Visibility,
+    pub name: Ident,
+    pub trait_name: Ident,
+    pub variants: Vec<VariantAST>,
+}
+
+impl Parse for EnumAST {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let attrs = Attribute::parse_outer(input)?;
+        let vis = input.parse()?;
+        input.parse::<Token![enum]>()?;
+        let name = input.parse()?;
+        input.parse::<Token![trait]>()?;
+        let trait_name = input.parse()?;
+        let content;
+        braced!(content in input);
+        let mut variants = Vec::new();
+        while !content.is_empty() {
+            variants.push(content.parse()?);
+        }
+        Ok(Self {
+            attrs,
+            vis,
+            name,
+            trait_name,
+            variants,
+        })
+    }
+}
+
+#[derive(Debug)]
+pub struct VariantAST {
+    pub name: Ident,
+    pub sequence: SequenceAST,
+}
+
+impl Parse for VariantAST {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let name = input.parse()?;
+        let sequence = input.parse()?;
+        Ok(Self { name, sequence })
+    }
 }
 
 #[derive(Debug)]
@@ -85,38 +128,94 @@ pub struct LambdaAST {
     pub entry: EntryAST,
 }
 
+impl Parse for LambdaAST {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let name = input.parse()?;
+        let entry = input.parse()?;
+        Ok(Self { name, entry })
+    }
+}
+
 #[derive(Debug)]
 pub enum SequenceAST {
-    Object(Vec<(Ident, EntryAST)>),
-    Tuple(Vec<EntryAST>),
+    Object(ObjectSequenceAST),
+    Tuple(TupleSequenceAST),
 }
 
 impl Parse for SequenceAST {
     fn parse(input: ParseStream) -> syn::Result<Self> {
-        let lookahead1 = input.lookahead1();
-        if lookahead1.peek(Brace) {
-            let content;
-            braced!(content in input);
-            let mut entries = Vec::new();
-            while !content.is_empty() {
-                let name = content.parse()?;
-                content.parse::<Token![:]>()?;
-                let entry = content.parse()?;
-                entries.push((name, entry));
-            }
-            Ok(Self::Object(entries))
-        } else if lookahead1.peek(Paren) {
-            let content;
-            parenthesized!(content in input);
-            let mut entries = Vec::new();
-            while !content.is_empty() {
-                let entry = content.parse()?;
-                entries.push(entry);
-            }
-            Ok(Self::Tuple(entries))
-        } else {
-            Err(lookahead1.error())
+        let fork = input.fork();
+        if let Ok(object) = fork.parse::<ObjectSequenceAST>() {
+            input.advance_to(&fork);
+            return Ok(Self::Object(object));
         }
+        let fork = input.fork();
+        if let Ok(tuple) = fork.parse::<TupleSequenceAST>() {
+            input.advance_to(&fork);
+            return Ok(Self::Tuple(tuple));
+        }
+        Err(input.error("expected object or tuple sequence"))
+    }
+}
+
+#[derive(Debug)]
+pub struct ObjectSequenceAST {
+    pub fields: Vec<ObjectFieldAST>,
+}
+
+impl Parse for ObjectSequenceAST {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let content;
+        braced!(content in input);
+        let mut fields = Vec::new();
+        while !content.is_empty() {
+            fields.push(content.parse()?);
+        }
+        Ok(Self { fields })
+    }
+}
+
+#[derive(Debug)]
+pub struct ObjectFieldAST {
+    pub name: Ident,
+    pub entry: EntryAST,
+}
+
+impl Parse for ObjectFieldAST {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let name = input.parse()?;
+        input.parse::<Token![:]>()?;
+        let entry = input.parse()?;
+        Ok(Self { name, entry })
+    }
+}
+
+#[derive(Debug)]
+pub struct TupleSequenceAST {
+    pub fields: Vec<TupleFieldAST>,
+}
+
+impl Parse for TupleSequenceAST {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let content;
+        parenthesized!(content in input);
+        let mut fields = Vec::new();
+        while !content.is_empty() {
+            fields.push(content.parse()?);
+        }
+        Ok(Self { fields })
+    }
+}
+
+#[derive(Debug)]
+pub struct TupleFieldAST {
+    pub entry: EntryAST,
+}
+
+impl Parse for TupleFieldAST {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let entry = input.parse()?;
+        Ok(Self { entry })
     }
 }
 
@@ -129,47 +228,11 @@ pub struct EntryAST {
 impl Parse for EntryAST {
     fn parse(input: ParseStream) -> syn::Result<Self> {
         let factor = input.parse()?;
-        let quantifier = 'quant: {
-            let repeater = if input.parse::<Option<Token![+]>>()?.is_some() {
-                RepeaterAST::Plus
-            } else if input.parse::<Option<Token![*]>>()?.is_some() {
-                RepeaterAST::Star
-            } else if input.parse::<Option<Token![?]>>()?.is_some() {
-                RepeaterAST::Option
-            } else if input.peek(Bracket) {
-                let content;
-                bracketed!(content in input);
-                let lookahead1 = content.lookahead1();
-                if lookahead1.peek(LitInt) {
-                    if content.peek2(Token![,]) {
-                        if content.peek3(LitInt) {
-                            let min: LitInt = content.parse()?;
-                            content.parse::<Token![,]>()?;
-                            let max: LitInt = content.parse()?;
-                            RepeaterAST::Range(min, max)
-                        } else {
-                            let min: LitInt = content.parse()?;
-                            content.parse::<Token![,]>()?;
-                            RepeaterAST::Min(min)
-                        }
-                    } else {
-                        let val: LitInt = content.parse()?;
-                        RepeaterAST::Val(val)
-                    }
-                } else if lookahead1.peek(Token![,]) {
-                    content.parse::<Token![,]>()?;
-                    let max: LitInt = content.parse()?;
-                    RepeaterAST::Max(max)
-                } else {
-                    return Err(lookahead1.error());
-                }
-            } else {
-                break 'quant None;
-            };
-            let lazy = input.parse::<Option<Token![?]>>()?.is_some();
-            Some(QuantifierAST { repeater, lazy })
-        };
-
+        let fork = input.fork();
+        let quantifier = fork.parse().ok().map(|quantifier| {
+            input.advance_to(&fork);
+            quantifier
+        });
         Ok(Self { factor, quantifier })
     }
 }
@@ -185,20 +248,26 @@ pub enum FactorAST {
 
 impl Parse for FactorAST {
     fn parse(input: ParseStream) -> syn::Result<Self> {
-        let lookahead1 = input.lookahead1();
-        if lookahead1.peek(Ident) {
-            Ok(Self::Shard(input.parse()?))
-        } else if lookahead1.peek(LitStr) {
-            Ok(Self::LitStr(input.parse()?))
-        } else if lookahead1.peek(LitChar) {
-            Ok(Self::LitChar(input.parse()?))
-        } else if lookahead1.peek(Brace) {
-            Ok(Self::Set(input.parse()?))
-        } else if lookahead1.peek(Paren) {
-            Ok(Self::Term(input.parse()?))
-        } else {
-            Err(lookahead1.error())
+        if input.peek(Ident) {
+            return Ok(Self::Shard(input.parse()?));
         }
+        if input.peek(LitStr) {
+            return Ok(Self::LitStr(input.parse()?));
+        }
+        if input.peek(LitChar) {
+            return Ok(Self::LitChar(input.parse()?));
+        }
+        let fork = input.fork();
+        if let Ok(set) = fork.parse::<SetAST>() {
+            input.advance_to(&fork);
+            return Ok(Self::Set(set));
+        }
+        let fork = input.fork();
+        if let Ok(term) = fork.parse::<TermAST>() {
+            input.advance_to(&fork);
+            return Ok(Self::Term(term));
+        }
+        Err(input.error("expected shard, literal string, literal char, set, or term"))
     }
 }
 
@@ -206,6 +275,19 @@ impl Parse for FactorAST {
 pub struct QuantifierAST {
     pub repeater: RepeaterAST,
     pub lazy: bool,
+}
+
+impl Parse for QuantifierAST {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let repeater = input.parse()?;
+        let lazy = if input.peek(Token![?]) {
+            input.parse::<Token![?]>()?;
+            true
+        } else {
+            false
+        };
+        Ok(Self { repeater, lazy })
+    }
 }
 
 #[derive(Debug)]
@@ -219,30 +301,48 @@ pub enum RepeaterAST {
     Range(LitInt, LitInt),
 }
 
-#[derive(Debug)]
-pub struct TermAST {
-    pub alts: Vec<Vec<EntryAST>>,
-}
-
-impl Parse for TermAST {
+impl Parse for RepeaterAST {
     fn parse(input: ParseStream) -> syn::Result<Self> {
-        let content;
-        parenthesized!(content in input);
-        content.parse::<Option<Token![|]>>()?;
-        let mut alts = Vec::new();
-        let mut entries = Vec::new();
-        while !content.is_empty() {
-            if content.parse::<Option<Token![|]>>()?.is_some() {
-                alts.push(entries);
-                entries = Vec::new();
+        let lookahead1 = input.lookahead1();
+        if lookahead1.peek(Token![+]) {
+            input.parse::<Token![+]>()?;
+            Ok(RepeaterAST::Plus)
+        } else if lookahead1.peek(Token![*]) {
+            input.parse::<Token![*]>()?;
+            Ok(RepeaterAST::Star)
+        } else if lookahead1.peek(Token![?]) {
+            input.parse::<Token![?]>()?;
+            Ok(RepeaterAST::Option)
+        } else if lookahead1.peek(Bracket) {
+            let content;
+            bracketed!(content in input);
+            let lookahead1 = content.lookahead1();
+            if lookahead1.peek(LitInt) {
+                if content.peek2(Token![,]) {
+                    if content.peek3(LitInt) {
+                        let min = content.parse()?;
+                        content.parse::<Token![,]>()?;
+                        let max = content.parse()?;
+                        Ok(RepeaterAST::Range(min, max))
+                    } else {
+                        let min = content.parse()?;
+                        content.parse::<Token![,]>()?;
+                        Ok(RepeaterAST::Min(min))
+                    }
+                } else {
+                    let val = content.parse()?;
+                    Ok(RepeaterAST::Val(val))
+                }
+            } else if lookahead1.peek(Token![,]) {
+                content.parse::<Token![,]>()?;
+                let max = content.parse()?;
+                Ok(RepeaterAST::Max(max))
             } else {
-                entries.push(content.parse()?);
+                return Err(lookahead1.error());
             }
+        } else {
+            Err(lookahead1.error())
         }
-        if !entries.is_empty() {
-            alts.push(entries);
-        }
-        Ok(Self { alts })
     }
 }
 
@@ -256,7 +356,12 @@ impl Parse for SetAST {
     fn parse(input: ParseStream) -> syn::Result<Self> {
         let content;
         braced!(content in input);
-        let exclusion = content.parse::<Option<Token![!]>>()?.is_some();
+        let exclusion = if content.peek(Token![!]) {
+            content.parse::<Token![!]>()?;
+            true
+        } else {
+            false
+        };
         let mut entries = Vec::new();
         while !content.is_empty() {
             entries.push(content.parse()?)
@@ -274,11 +379,51 @@ pub enum SetEntryAST {
 impl Parse for SetEntryAST {
     fn parse(input: ParseStream) -> syn::Result<Self> {
         let start = input.parse()?;
-        if input.parse::<Option<Token![..]>>()?.is_some() {
+        if input.peek(Token![..]) {
+            input.parse::<Token![..]>()?;
             let end = input.parse()?;
             Ok(Self::Range(start, end))
         } else {
             Ok(Self::Single(start))
         }
+    }
+}
+
+#[derive(Debug)]
+pub struct TermAST {
+    pub alts: Vec<TermAltAST>,
+}
+
+impl Parse for TermAST {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let content;
+        parenthesized!(content in input);
+        let mut alts = Vec::new();
+        while !content.is_empty() {
+            alts.push(content.parse()?);
+        }
+        Ok(Self { alts })
+    }
+}
+
+#[derive(Debug)]
+pub struct TermAltAST {
+    pub entries: Vec<EntryAST>,
+}
+
+impl Parse for TermAltAST {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        input.parse::<Token![|]>()?;
+        let mut entries = Vec::new();
+        while !input.is_empty() {
+            let fork = input.fork();
+            if let Ok(entry) = fork.parse() {
+                input.advance_to(&fork);
+                entries.push(entry);
+                continue;
+            }
+            break;
+        }
+        Ok(Self { entries })
     }
 }
--- crates/core/ui/proc/src/lib.rs
+++ crates/core/ui/proc/src/lib.rs
@@ -1,12 +1,10 @@
 mod ast;
 mod hir;
 
-use proc_macro2::TokenStream;
 use quote::quote;
-use rustc_hash::FxHashMap;
 use syn::parse;
 
-use crate::ast::{ClusterAST, FactorAST, RepeaterAST, SequenceAST, ShapeAST, ShardAST};
+use crate::ast::ClusterAST;
 
 #[proc_macro]
 pub fn cluster(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
@@ -14,72 +12,5 @@ pub fn cluster(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
         Ok(cluster) => cluster,
         Err(err) => return err.to_compile_error().into(),
     };
-    let shards = cluster.shards.iter().map(gen_shard);
-    quote! { #(#shards)* }.into()
-}
-
-fn gen_shard(shard: &ShardAST) -> TokenStream {
-    let attrs = &shard.attrs;
-    let vis = &shard.vis;
-    let name = &shard.name;
-    let trait_name = &shard.trait_name;
-    let keyword;
-    let def;
-    match &shard.shape {
-        ShapeAST::Struct(sequence) => {
-            keyword = quote! { struct };
-            def = match sequence {
-                SequenceAST::Object(entries) => {
-                    let mut grouped_entries: FxHashMap<_, Vec<_>> = FxHashMap::default();
-                    for (name, entry) in entries.iter() {
-                        let type_ = match &entry.factor {
-                            FactorAST::Shard(ident) => quote! { #ident },
-                            FactorAST::LitStr(str) => quote! { () },
-                            FactorAST::LitChar(char) => quote! { () },
-                            FactorAST::Set(set) => quote! { () },
-                            FactorAST::Term(term) => quote! { () },
-                        };
-                        let type_ = match &entry.quantifier {
-                            Some(quantifier) => match &quantifier.repeater {
-                                RepeaterAST::Plus => quote! { Vec<#type_> },
-                                RepeaterAST::Star => quote! { Vec<#type_> },
-                                RepeaterAST::Option => quote! { Option<#type_> },
-                                RepeaterAST::Val(val) => quote! { [#type_; #val] },
-                                RepeaterAST::Min(_) => quote! { Vec<#type_> },
-                                RepeaterAST::Max(_) => quote! { Vec<#type_> },
-                                RepeaterAST::Range(_, _) => quote! { Vec<#type_> },
-                            },
-                            None => type_,
-                        };
-                        grouped_entries.entry(name).or_default().push(type_);
-                    }
-                    let entries = grouped_entries.iter().map(|(name, types)| {
-                        quote! { #name: (#(#types),*) }
-                    });
-                    quote! { where Self: #trait_name { #(#entries),* } }
-                }
-                SequenceAST::Tuple(entries) => {
-                    quote! { () where Self: #trait_name; }
-                }
-            };
-        }
-        ShapeAST::Enum(variants) => {
-            keyword = quote! { enum };
-            let variants = variants.iter().map(|(name, sequence)| match sequence {
-                SequenceAST::Object(entries) => {
-                    quote! { #name {} }
-                }
-                SequenceAST::Tuple(entries) => {
-                    quote! { #name () }
-                }
-            });
-            def = quote! { where Self: #trait_name { #(#variants),* } };
-        }
-    };
-    quote! {
-        #(#attrs)*
-        #vis #keyword #name #def
-        trait #trait_name {}
-        impl ::aeris::ui::Shard for #name {}
-    }
+    quote! {}.into()
 }
