f326356ad3d4eaf5b64be90d164321504ff0746d
PhillY <philly706869@gmail.com>
2026-08-09 19:03:05 +0000

    2026-08-10 2

--- crates/core/ui/proc/src/ir.rs
+++ crates/core/ui/proc/src/ir.rs
@@ -1,78 +1,78 @@
-use proc_macro2::TokenStream;
-use quote::quote;
-use rustc_hash::FxHashMap;
+// use proc_macro2::TokenStream;
+// use quote::quote;
+// use rustc_hash::FxHashMap;
 
-use crate::ast::{ClusterAST, ShardAST};
+// use crate::ast::{ClusterAST, Shard};
 
-#[derive(Debug)]
-pub struct ClusterIR {
-    struct_shards: Vec<StructShardIR>,
-    enum_shards: Vec<EnumShardIR>,
-    lambda_shards: Vec<LambdaShardIR>,
-}
+// #[derive(Debug)]
+// pub struct ClusterIR {
+//     struct_shards: Vec<StructShardIR>,
+//     enum_shards: Vec<EnumShardIR>,
+//     lambda_shards: Vec<LambdaShardIR>,
+// }
 
-impl ClusterIR {
-    pub fn lower(ast: &ClusterAST) -> syn::Result<Self> {
-        let mut struct_shards = Vec::new();
-        let mut enum_shards = Vec::new();
-        let mut lambda_shards = Vec::new();
-        let mut name_table = FxHashMap::default();
-        let mut name_collisions = Vec::new();
-        for shard in &ast.shards {
-            let name_ident = match shard {
-                ShardAST::Struct(shard) => {
-                    let node = StructShardIR {};
-                    struct_shards.push(node);
-                    &shard.name
-                }
-                ShardAST::Enum(shard) => {
-                    let node = EnumShardIR {};
-                    enum_shards.push(node);
-                    &shard.name
-                }
-                ShardAST::Lambda(shard) => {
-                    let node = LambdaShardIR {};
-                    lambda_shards.push(node);
-                    &shard.name
-                }
-            };
-            let name_str = name_ident.to_string();
-            let collision = name_table.insert(name_str, name_ident);
-            if let Some(collision) = collision {
-                name_collisions.push(collision);
-                name_collisions.push(name_ident);
-            }
-        }
-        if let Some(err) = name_collisions
-            .into_iter()
-            .map(|name| {
-                let msg = format!("duplicate shard name {}", name.to_string());
-                syn::Error::new_spanned(name, msg)
-            })
-            .reduce(|mut acc, curr| {
-                acc.combine(curr);
-                acc
-            })
-        {
-            return Err(err);
-        }
-        Ok(Self {
-            struct_shards,
-            enum_shards,
-            lambda_shards,
-        })
-    }
+// impl ClusterIR {
+//     pub fn lower(ast: &ClusterAST) -> syn::Result<Self> {
+//         let mut struct_shards = Vec::new();
+//         let mut enum_shards = Vec::new();
+//         let mut lambda_shards = Vec::new();
+//         let mut name_table = FxHashMap::default();
+//         let mut name_collisions = Vec::new();
+//         for shard in &ast.shards {
+//             let name_ident = match shard {
+//                 Shard::Struct(shard) => {
+//                     let node = StructShardIR {};
+//                     struct_shards.push(node);
+//                     &shard.name
+//                 }
+//                 Shard::Enum(shard) => {
+//                     let node = EnumShardIR {};
+//                     enum_shards.push(node);
+//                     &shard.name
+//                 }
+//                 Shard::Lambda(shard) => {
+//                     let node = LambdaShardIR {};
+//                     lambda_shards.push(node);
+//                     &shard.name
+//                 }
+//             };
+//             let name_str = name_ident.to_string();
+//             let collision = name_table.insert(name_str, name_ident);
+//             if let Some(collision) = collision {
+//                 name_collisions.push(collision);
+//                 name_collisions.push(name_ident);
+//             }
+//         }
+//         if let Some(err) = name_collisions
+//             .into_iter()
+//             .map(|name| {
+//                 let msg = format!("duplicate shard name {}", name.to_string());
+//                 syn::Error::new_spanned(name, msg)
+//             })
+//             .reduce(|mut acc, curr| {
+//                 acc.combine(curr);
+//                 acc
+//             })
+//         {
+//             return Err(err);
+//         }
+//         Ok(Self {
+//             struct_shards,
+//             enum_shards,
+//             lambda_shards,
+//         })
+//     }
 
-    pub fn expand(&self) -> TokenStream {
-        quote! {}
-    }
-}
+//     pub fn expand(&self) -> TokenStream {
+//         quote! {}
+//     }
+// }
 
-#[derive(Debug)]
-pub struct StructShardIR {}
+// #[derive(Debug)]
+// pub struct StructShardIR {}
 
-#[derive(Debug)]
-pub struct EnumShardIR {}
+// #[derive(Debug)]
+// pub struct EnumShardIR {}
 
-#[derive(Debug)]
-pub struct LambdaShardIR {}
+// #[derive(Debug)]
+// pub struct LambdaShardIR {}
--- crates/core/ui/proc/src/lib.rs
+++ crates/core/ui/proc/src/lib.rs
@@ -3,7 +3,7 @@ mod ir;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{Item, ItemMod, MacroDelimiter, Type, Visibility, parse, spanned::Spanned};
+use syn::parse;
 
 #[proc_macro_attribute]
 pub fn shard(attr: TokenStream, item: TokenStream) -> TokenStream {
@@ -17,185 +17,11 @@ pub fn shard(attr: TokenStream, item: TokenStream) -> TokenStream {
         ));
     }
 
-    match parse::<ItemMod>(item) {
-        Ok(module) => {
-            if let Some(unsafety) = module.unsafety {
-                errs.push(syn::Error::new_spanned(
-                    unsafety,
-                    "unsafe shard is not supported",
-                ))
-            }
-
-            errs.extend(
-                module
-                    .attrs
-                    .into_iter()
-                    .map(|attr| syn::Error::new_spanned(attr, "attribute is not supported")),
-            );
-
-            match (module.content, module.semi) {
-                (Some((_, items)), None) => {
-                    for item in items {
-                        match item {
-                            Item::Struct(item) => {
-                                if item.ident != "Shard" {
-                                    errs.push(syn::Error::new_spanned(item, "struct declaration except with name Shard is not supported in shard"));
-                                } else {
-                                    if item.vis != Visibility::Inherited {
-                                        errs.push(syn::Error::new_spanned(
-                                            &item.vis,
-                                            "visibility is not supported in shard",
-                                        ));
-                                    }
-                                    if !item.generics.params.is_empty() {
-                                        errs.push(syn::Error::new_spanned(
-                                            &item.generics,
-                                            "generic is not supported in shard",
-                                        ));
-                                    }
-                                    if item.generics.where_clause.is_some() {
-                                        errs.push(syn::Error::new_spanned(
-                                            &item.generics.where_clause,
-                                            "where clause is not supported in shard",
-                                        ));
-                                    }
-                                }
-                            }
-                            Item::Enum(item) => {
-                                if item.ident != "Shard" {
-                                    errs.push(syn::Error::new_spanned(item, "enum declaration except with name Shard is not supported in shard"));
-                                } else {
-                                    if item.vis != Visibility::Inherited {
-                                        errs.push(syn::Error::new_spanned(
-                                            &item.vis,
-                                            "visibility is not supported in shard",
-                                        ));
-                                    }
-                                    if !item.generics.params.is_empty() {
-                                        errs.push(syn::Error::new_spanned(
-                                            &item.generics,
-                                            "generic is not supported in shard",
-                                        ));
-                                    }
-                                    if item.generics.where_clause.is_some() {
-                                        errs.push(syn::Error::new_spanned(
-                                            &item.generics.where_clause,
-                                            "where clause is not supported in shard",
-                                        ));
-                                    }
-                                }
-                            }
-                            Item::Impl(item) => match item.self_ty.as_ref() {
-                                Type::Path(path)
-                                    if {
-                                        let segments = &path.path.segments;
-                                        segments.len() == 1 && segments[0].ident == "Shard"
-                                    } =>
-                                {
-                                    if let Some(unsafety) = item.unsafety {
-                                        errs.push(syn::Error::new_spanned(
-                                            unsafety,
-                                            "unsafe is not supported in shard",
-                                        ));
-                                    }
-                                    if !item.generics.params.is_empty() {
-                                        errs.push(syn::Error::new_spanned(
-                                            &item.generics,
-                                            "generic is not supported in shard",
-                                        ));
-                                    }
-                                    if let Some(where_clause) = item.generics.where_clause {
-                                        errs.push(syn::Error::new_spanned(
-                                            where_clause,
-                                            "where clause is not supported in shard",
-                                        ));
-                                    }
-                                }
-                                _ => {
-                                    errs.push(syn::Error::new_spanned(item, "impl declaration except with name Shard is not supported in shard"));
-                                }
-                            },
-                            Item::Macro(item) => {
-                                if let Some(ident) = item.ident {
-                                    errs.push(syn::Error::new_spanned(
-                                        ident,
-                                        "ident is not supported in shard",
-                                    ));
-                                }
-                                match item.mac.delimiter {
-                                    MacroDelimiter::Brace(_) => {}
-                                    MacroDelimiter::Bracket(bracket) => {
-                                        errs.push(syn::Error::new(
-                                            bracket.span.span(),
-                                            "", // TODO
-                                        ));
-                                    }
-                                    MacroDelimiter::Paren(paren) => {
-                                        errs.push(syn::Error::new(
-                                            paren.span.span(),
-                                            "", // TODO
-                                        ));
-                                    }
-                                }
-                            }
-                            Item::Const(item) => errs.push(syn::Error::new_spanned(
-                                item,
-                                "const declaration is not supported in shard",
-                            )),
-                            Item::ExternCrate(item) => errs.push(syn::Error::new_spanned(
-                                item,
-                                "extern crate declaration is not supported in shard",
-                            )),
-                            Item::Fn(item) => errs.push(syn::Error::new_spanned(
-                                item,
-                                "function declaration is not supported in shard",
-                            )),
-                            Item::ForeignMod(item) => errs.push(syn::Error::new_spanned(
-                                item,
-                                "extern module declaration is not supported in shard",
-                            )),
-                            Item::Mod(item) => errs.push(syn::Error::new_spanned(
-                                item,
-                                "module declaration is not supported in shard",
-                            )),
-                            Item::Static(item) => errs.push(syn::Error::new_spanned(
-                                item,
-                                "static declaration is not supported in shard",
-                            )),
-                            Item::Trait(item) => errs.push(syn::Error::new_spanned(
-                                item,
-                                "trait declaration is not supported in shard",
-                            )),
-                            Item::TraitAlias(item) => errs.push(syn::Error::new_spanned(
-                                item,
-                                "trait alias declaration is not supported in shard",
-                            )),
-                            Item::Type(item) => errs.push(syn::Error::new_spanned(
-                                item,
-                                "type alias declaration is not supported in shard",
-                            )),
-                            Item::Union(item) => errs.push(syn::Error::new_spanned(
-                                item,
-                                "union declaration is not supported in shard",
-                            )),
-                            Item::Use(item) => errs.push(syn::Error::new_spanned(
-                                item,
-                                "use declaration is not supported in shard",
-                            )),
-                            _ => errs.push(syn::Error::new_spanned(
-                                item,
-                                "item is not supported in shard",
-                            )),
-                        }
-                    }
-                }
-                (None, Some(semi)) => {
-                    errs.push(syn::Error::new_spanned(semi, "unexpected token"));
-                }
-                _ => unreachable!(),
-            }
+    match parse::<ast::Shard>(item) {
+        Ok(shard) => {}
+        Err(err) => {
+            errs.push(err);
         }
-        Err(err) => errs.push(err),
     }
 
     match errs.into_iter().reduce(|mut acc, curr| {
