ea39d9b86e124c34d1d88727a14eef02404e09ea
PhillY <philly706869@gmail.com>
2026-08-10 00:42:02 +0000

    2026-08-10 4

---
+++ crates/core/ui/proc/src/shard.rs
@@ -0,0 +1,21 @@
+use proc_macro2::TokenStream;
+use syn::{ItemMod, parse2};
+
+pub fn shard(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let attr_err = (!attr.is_empty()).then_some(syn::Error::new_spanned(
+        attr,
+        "unexpected attribute argument",
+    ));
+
+    let module: ItemMod = match parse2(item) {
+        Ok(module) => module,
+        Err(mut err) => {
+            if let Some(attr_err) = attr_err {
+                err.combine(attr_err);
+            }
+            return err.into_compile_error();
+        }
+    };
+
+    TokenStream::new()
+}
