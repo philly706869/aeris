e9cdfec9d74d6d4ad4802cf9c0f6ef65c858f79f
PhillY <philly706869@gmail.com>
2026-08-30 11:21:46 +0000

    2026-08-30 1

--- crates/core/ui/lib/src/lib.rs
+++ crates/core/ui/lib/src/lib.rs
@@ -1,14 +1,34 @@
-use std::any::TypeId;
+use std::{any::TypeId, ops::RangeInclusive};
 
-pub trait Shard
-where
-    Self::TypeId: 'static,
-{
-    type TypeId;
+pub trait Shard {
+    const TUID: TypeId;
     const DATA: &'static ShardData;
 }
 
-pub enum ShardData {}
+mod sealed {
+    pub trait Sealed {}
+}
+
+use sealed::Sealed;
+
+pub trait ShardEntry
+where
+    Self: Sealed,
+{
+}
+
+impl<T> Sealed for T where T: Shard {}
+impl<T> ShardEntry for T where T: Shard {}
+
+impl<'t> Sealed for &'t str {}
+impl<'t> ShardEntry for &'t str {}
+
+pub enum ShardData {
+    Literal(&'static str),
+    Set(bool, Vec<RangeInclusive<char>>),
+    Sequence(Vec<ShardData>),
+    Alternative(Vec<ShardData>),
+}
 
 pub const fn build<S>()
 where
--- examples/json/src/cluster/array.rs
+++ examples/json/src/cluster/array.rs
@@ -16,37 +16,111 @@ pub struct Punctuated<T, P> {
 }
 
 mod mapping {
-    use std::marker::PhantomData;
-
     #[derive(Debug)]
     pub struct WS<'i> {
-        _i: PhantomData<&'i Self>,
+        _i: ::std::marker::PhantomData<&'i Self>,
         space: &'i str,
     }
 
+    const _: () = {
+        use ::aeris::ui::Shard;
+        use ::aeris::ui::ShardData;
+        use ::std::any::TypeId;
+        struct STATIC;
+        static SHARD_DATA: ShardData = ShardData::Literal("");
+        impl<'i> Shard for WS<'i> {
+            const TUID: TypeId = TypeId::of::<STATIC>();
+            const DATA: &'static ShardData = &SHARD_DATA;
+        }
+    };
+
     #[derive(Debug)]
-    pub struct Punctuated<'i, T, P> {
-        _i: PhantomData<&'i Self>,
+    pub struct Punctuated<'i, T, P>
+    where
+        T: ::aeris::ui::ShardEntry,
+        P: ::aeris::ui::ShardEntry,
+    {
+        _i: ::std::marker::PhantomData<&'i Self>,
         inner: Option<(T, Vec<(P, T)>)>,
     }
 
+    const _: () = {
+        use ::aeris::ui::Shard;
+        use ::aeris::ui::ShardData;
+        use ::aeris::ui::ShardEntry;
+        use ::std::any::TypeId;
+        struct STATIC;
+        static SHARD_DATA: ShardData = ShardData::Literal("");
+        impl<'i, T, P> Shard for Punctuated<'i, T, P>
+        where
+            T: ShardEntry,
+            P: ShardEntry,
+        {
+            const TUID: TypeId = TypeId::of::<STATIC>();
+            const DATA: &'static ShardData = &SHARD_DATA;
+        }
+    };
+
     #[derive(Debug)]
-    pub struct Spanned<'i, T> {
-        _i: PhantomData<&'i Self>,
+    pub struct Spanned<'i, T>
+    where
+        T: ::aeris::ui::ShardEntry,
+    {
+        _i: ::std::marker::PhantomData<&'i Self>,
         inner: T,
         ws: WS<'i>,
     }
 
+    const _: () = {
+        use ::aeris::ui::Shard;
+        use ::aeris::ui::ShardData;
+        use ::aeris::ui::ShardEntry;
+        use ::std::any::TypeId;
+        struct STATIC;
+        static SHARD_DATA: ShardData = ShardData::Literal("");
+        impl<'i, T> Shard for Spanned<'i, T>
+        where
+            T: ShardEntry,
+        {
+            const TUID: TypeId = TypeId::of::<STATIC>();
+            const DATA: &'static ShardData = &SHARD_DATA;
+        }
+    };
+
     #[derive(Debug)]
     pub struct JSONArray<'i> {
-        _i: PhantomData<&'i Self>,
+        _i: ::std::marker::PhantomData<&'i Self>,
         bracket: (&'i str, &'i str),
         ws: WS<'i>,
         entries: Punctuated<'i, Spanned<'i, JSONValue<'i>>, Spanned<'i, &'i str>>,
     }
 
+    const _: () = {
+        use ::aeris::ui::Shard;
+        use ::aeris::ui::ShardData;
+        use ::std::any::TypeId;
+        struct STATIC;
+        static SHARD_DATA: ShardData = ShardData::Literal("");
+        impl<'i> Shard for JSONArray<'i> {
+            const TUID: TypeId = TypeId::of::<STATIC>();
+            const DATA: &'static ShardData = &SHARD_DATA;
+        }
+    };
+
     #[derive(Debug)]
     pub struct JSONValue<'i> {
-        _i: PhantomData<&'i Self>,
+        _i: ::std::marker::PhantomData<&'i Self>,
     }
+
+    const _: () = {
+        use ::aeris::ui::Shard;
+        use ::aeris::ui::ShardData;
+        use ::std::any::TypeId;
+        struct STATIC;
+        static SHARD_DATA: ShardData = ShardData::Literal("");
+        impl<'i> Shard for JSONValue<'i> {
+            const TUID: TypeId = TypeId::of::<STATIC>();
+            const DATA: &'static ShardData = &SHARD_DATA;
+        }
+    };
 }
