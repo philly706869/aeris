618456f8942a66528256a2ad4c9ce70db418ac65
PhillY <philly706869@gmail.com>
2026-09-12 18:34:36 +0000

    2026-09-13 10

--- crates/xst/proc/src/shard.rs
+++ crates/xst/proc/src/shard.rs
@@ -1,4 +1,5 @@
 use proc_macro2::TokenStream;
+use quote::quote;
 use syn::parse2;
 
 mod ast;
@@ -20,8 +21,115 @@ fn expand_attr(attr: TokenStream) -> TokenStream {
 }
 
 fn expand_item(item: TokenStream) -> TokenStream {
-    match parse2::<ast::Shard>(item) {
-        Ok(_) => TokenStream::new(),
-        Err(err) => err.into_compile_error(),
+    let ast: ast::Shard = match parse2(item) {
+        Ok(ast) => ast,
+        Err(err) => return err.into_compile_error(),
+    };
+
+    use ast::rust_expr::{Expr, keyword};
+
+    enum Magic {
+        X(keyword::x),
+        XBox(keyword::xbox),
+        XOpt(keyword::xopt),
+        XVec(keyword::xvec),
+        XLOpt(keyword::xlopt),
+        XLVec(keyword::xlvec),
+    }
+
+    fn expand_magics(magics: &mut Vec<Magic>, expr: &Expr) {
+        let mut stack = vec![expr];
+        while let Some(item) = stack.pop() {
+            match item {
+                Expr::X(item) => {
+                    magics.push(Magic::X(item.x_token));
+                }
+                Expr::XBox(item) => {
+                    magics.push(Magic::XBox(item.xbox_token));
+                    stack.push(&item.expr);
+                }
+                Expr::XOpt(item) => {
+                    magics.push(Magic::XOpt(item.xopt_token));
+                    stack.push(&item.expr);
+                }
+                Expr::XVec(item) => {
+                    magics.push(Magic::XVec(item.xvec_token));
+                    stack.push(&item.expr);
+                }
+                Expr::XLOpt(item) => {
+                    magics.push(Magic::XLOpt(item.xlopt_token));
+                    stack.push(&item.expr);
+                }
+                Expr::XLVec(item) => {
+                    magics.push(Magic::XLVec(item.xlvec_token));
+                    stack.push(&item.expr);
+                }
+                Expr::Tuple(item) => {
+                    stack.extend(item.exprs.iter());
+                }
+                Expr::Shard(item) => {
+                    stack.extend(item.args.exprs.iter());
+                }
+            }
+        }
+    }
+
+    let mut magics = Vec::new();
+
+    match ast {
+        ast::Shard::Struct(item) => {
+            for field in item.fields {
+                expand_magics(&mut magics, &field.expr);
+            }
+        }
+        ast::Shard::Enum(item) => {
+            for variant in item.variants.iter() {
+                for expr in variant.shard_expr.args.exprs.iter() {
+                    expand_magics(&mut magics, expr);
+                }
+            }
+        }
+        ast::Shard::Type(item) => {
+            magics.push(Magic::X(item.x_expr.x_token));
+        }
+    }
+
+    let magics = magics.iter().map(|magic| match magic {
+        Magic::X(token) => quote! { #token! {} },
+        Magic::XBox(token) => quote! { #token! []; },
+        Magic::XOpt(token) => quote! { #token! []; },
+        Magic::XVec(token) => quote! { #token! []; },
+        Magic::XLOpt(token) => quote! { #token! []; },
+        Magic::XLVec(token) => quote! { #token! []; },
+    });
+
+    quote! {
+        const _: () = {
+            /// # XST x!
+            macro_rules! x {
+                {} => {}
+            }
+            /// # XST xbox!
+            macro_rules! xbox {
+                [] => {}
+            }
+            /// # XST xopt!
+            macro_rules! xopt {
+                [] => {}
+            }
+            /// # XST xvec!
+            macro_rules! xvec {
+                [] => {}
+            }
+            /// # XST xlopt!
+            macro_rules! xlopt {
+                [] => {}
+            }
+            /// # XST xlvec!
+            macro_rules! xlvec {
+                [] => {}
+            }
+            #(#magics)*
+        };
     }
 }
